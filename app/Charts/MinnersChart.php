3dfb5e42e5fa7e1c359a03b2178d5e18d8ffb6a5
FabioPerez <fanpero87@gmail.com>
2021-07-19 23:25:01 +0000

    fixinf the date on the chart

--- app/Charts/MinnersChart.php
+++ app/Charts/MinnersChart.php
@@ -20,22 +20,29 @@ class MinnersChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
-
+        // Api call to get get the value of a BTC
         $getrate = "https://api.alternative.me/v2/ticker/bitcoin/?convert=USD";
         $price = file_get_contents($getrate);
         $result = json_decode($price, true);
         $btc2usd = $result['data'][1]['quotes']['USD']['price'];
 
-        sleep(1);
+        // sleep(1);
 
+        //Grab the date from the DB date and change the format
         $date = Minner::pluck('created_at')->toArray();
+        foreach($date as $key => $value)
+        {
+            $date[$key] = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('d/m/Y');
+        }
 
+        // Grab all the data from the DB and make it and array
         $payout = Minner::pluck('est_month_payment')->toArray();
         $balance = Minner::pluck('current_balance')->toArray();
         $m5a = Minner::pluck('m5a_est')->toArray();
         $x60a = Minner::pluck('x60a_est')->toArray();
         $x20a = Minner::pluck('x20a_est')->toArray();
 
+        // Convert the current balance from BTC to USD and make it an array
         $balanceInUSD=[];
         foreach($balance as $key => $value)
         {
--- app/Http/Controllers/MinnersController.php
+++ app/Http/Controllers/MinnersController.php
@@ -13,63 +13,7 @@ class MinnersController extends Controller
 {
     public function index(){
 
-        // $dates = Minner::all();
-
-        // foreach($dates as $date)
-        // {
-
-        // }
-        // $d['created_at'] = Carbon::createFromFormat('Y-m-d H:i:s', $d)->format('d/m/Y');
-        // dd($d);
-
-
-        // $a = Carbon::createFromFormat('Y-m-d H:i:s', $d)->format('d/m/Y');
-
-
-        // $d = Minner::pluck('created_at')->first();
-        // $a = $d->dateFormat();
-        // dd($d);
-        // foreach($d as  $row)
-        // {
-
-        // }
-        // foreach ($validated as $key => $value) {
-        //     $account->{$key} = $value;
-        // }
-
-        $response = Http::get('https://blockchain.info/ticker');
-        $value=1;
-        $toBtc = HTTP::get('https://blockchain.info/tobtc?currency=USD&value='.$value);
-
-
-
-        $getrate = "https://api.alternative.me/v2/ticker/bitcoin/?convert=USD";
-
-        $price = file_get_contents($getrate);
-        $result = json_decode($price, true);
-
-        // BTC in USD
-        $result = $result['data'][1]['quotes']['USD']['price'];
-
-         //Option 1
-        // $quantity = 0.00982701;
-        // $value = $quantity * $result;
-
-        // Option 2
-
-        $balance = Minner::pluck('current_balance')->toArray();
-        $allValues=[];
-
-        // dd($result);
-
-        foreach($balance as $key => $data)
-        {
-            $allValues[$key] = $data * $result;
-        }
-
-        // dd($allValues);
-
-        return view('welcome', compact('response', 'value'));
+        return view('welcome');
     }
 
     public function store(Request $request)
